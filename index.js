3d2e5911d58c5ab1fd2bc4ca677f1950be716487
ShinyACash <76147605+ShinyACash@users.noreply.github.com>
2022-01-13 13:21:07 +0530

    0.1
    beta, framework in place

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client;
+
+
+client.once("ready", () =>{
+    console.log("ready bitch.");
+})
+
+
+
+client.login("OTMwNzUyNzU1MTU1NDg0NzIz.Yd6dDQ.hojoG1XJ87iwbr-6DWs16AIEoew");
